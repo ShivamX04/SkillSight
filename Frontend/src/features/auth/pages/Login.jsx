ef5c776526545451884d5a4f99f4700ac00a3c64
shivam tiwari <st532288@gmail.com>
2026-04-13 22:11:59 +0530

    rebuild frontend

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -13,6 +13,8 @@ const Login = () => {
   const [email, setemail] = useState('')
   const [password, setpassword] = useState('')
 
+  console.log("LOGIN RESPONSE:", response.data);
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
