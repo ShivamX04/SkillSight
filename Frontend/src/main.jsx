95891523426c579dbf1aed34677245661f89edd7
shivam tiwari <st532288@gmail.com>
2026-04-14 12:38:35 +0530

    Initial commit - skillsight project

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.scss'
 import App from './App.jsx'
+import { AuthProvider } from './auth.context' // ✅ import
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+  <StrictMode>
+    <AuthProvider>   {/* ✅ WRAP HERE */}
+      <App />
+    </AuthProvider>
+  </StrictMode>
 )
- 
