9e0f68f09a0f9df037f68f8f5f58d90f5a682bf3
shivam tiwari <st532288@gmail.com>
2026-04-14 14:28:59 +0530

    Initial commit -skillsight project

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,18 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.scss'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-// ✅ IMPORTANT: correct path to your context
+// ✅ import RouterProvider
+import { RouterProvider } from "react-router-dom"
+import { router } from "./router" // your file
+
 import { AuthProvider } from './features/auth/auth.context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />  {/* ✅ THIS replaces BrowserRouter */}
     </AuthProvider>
   </StrictMode>
 )
